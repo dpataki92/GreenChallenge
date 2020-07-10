4d555ccdf07f8c5a57f282f4a2e8e7d716723685
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-07-10 16:40:47 +0200

    setting up routes for sessionscontroller methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    def home
+    end
+
+    def create
+    end
+
+    def signup_page
+    end
+
+    def signup_manual
+    end
+
+    def login_manual
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root "sessions#home"
+
   resources :users, only: [:index, :new, :create, :destroy]
+
+  #routes for SessionsController
+
+  get "/", to: "sessions#home"
+  get "/signup", to: "sessions#signup_page"
+  post "/signup", to: "sessions#signup_manual"
+  post "/login", to: "sessions#login_manual"
+  get '/auth/:provider/callback', to: 'sessions#create'
 end
