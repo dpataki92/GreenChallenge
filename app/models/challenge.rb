d882a449f18adda61fd9b60a015cfd267bd63df9
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-07-16 19:00:42 +0200

    including validations for user model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,7 +4,7 @@ class Challenge < ApplicationRecord
     has_many :users, through: :commitments
     has_and_belongs_to_many :groups
 
-
+    validates 
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
     has_many :posts
     has_many :comments
     
-
+    validates :name, presence: true
+    validates :email, presence: true, format: /.+@.+\.{1}.{2,}/
+    validates :password, length: { in: 5..20 }
 
 end
