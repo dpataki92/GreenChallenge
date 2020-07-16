8e215be06600f7015921260128dd62dcb16e7f24
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-07-16 19:20:04 +0200

    including validations for commitment and group models

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,6 @@ class Group < ApplicationRecord
     has_many :users, through: :memberships
     has_and_belongs_to_many :challenges
 
+    validates :name, presence: true
+    validates :description, presence: true
 end
