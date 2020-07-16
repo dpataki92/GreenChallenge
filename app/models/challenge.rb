18a211b7f014e64efd3bfc81357f1aa31ab4beb3
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-07-16 19:13:15 +0200

    including validations for challenge  model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,7 +4,8 @@ class Challenge < ApplicationRecord
     has_many :users, through: :commitments
     has_and_belongs_to_many :groups
 
-    validates 
-
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :description, length: { maximum: 500 }
 
 end
