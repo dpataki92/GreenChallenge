1c93905210a978819e548b294e70f6afd81882e5
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-07-20 15:59:02 +0200

    updating html and route for edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "sessions#home"
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
   # routes for SessionsController
     get "/", to: "sessions#home"
