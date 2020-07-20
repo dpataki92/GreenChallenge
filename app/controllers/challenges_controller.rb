2d0eb733598200506c4a2dc192c89330af5f3a87
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-07-20 22:45:01 +0200

    setting up basic controller logic and view for creating challenges and posts

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -19,11 +19,24 @@ class ChallengesController < ApplicationController
     end
 
     def create
+        @challenge = Challenge.new(challenge_params)
+
+        if @challenge.save
+            redirect_to challenges_path
+        else
+            render :index
+        end
     end
 
     def update
     end
     
     def destroy
+    end
+
+    private
+
+    def challenge_params
+        params.require(:challenge).permit(:title, :description)
     end
 end
