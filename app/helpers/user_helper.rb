f3a4e991a217494aebbfd4d0e6395b3890032e58
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-07-20 19:03:01 +0200

    adding user helper methods

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,4 +3,12 @@ module UserHelper
         avatar_array = ["/images/avatar0.png", "/images/avatar1.png", "/images/avatar2.png", "/images/avatar3.png", "/images/avatar5.png", "/images/avatar6.png", "/images/avatar7.jpg"]
     end
 
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
 end
