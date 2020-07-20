ce45fef60e28990361e57efd76f54930e1790116
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-07-20 16:21:47 +0200

    refactoring edit data and avatar page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,16 @@ class UsersController < ApplicationController
         @user = User.find_by(id: session[:user_id])
     end
 
+    def update
+        @user = User.find_by(id: session[:user_id])
+        @user.update(user_params)
+        redirect_to edit_user_path(@user)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :country, :avatar)
+    end
+
 end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,6 @@
 module UserHelper
     def avatar_selection
-        avatar_array = ["/images/avatar0.png", "/images/avatar1.png", "/images/avatar2.png", "/images/avatar3.png", "/images/avatar5.png", "/images/avatar6.png"]
+        avatar_array = ["/images/avatar0.png", "/images/avatar1.png", "/images/avatar2.png", "/images/avatar3.png", "/images/avatar5.png", "/images/avatar6.png", "/images/avatar7.jpg"]
     end
 
-    def create_hidden_field(a)
-        hidden_field_tag "user[avatar]", a
-    end
 end
